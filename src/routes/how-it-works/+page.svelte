9c80a3312d7708bceae136cf7a004d5971dd7a65
JustDaDoge <henryjiang2022@gmail.com>
2026-07-30 18:59:42 -0400

    some of how it works page
    I did like half of it. It is very uncomplete

---
+++ src/routes/how-it-works/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+	<title>How It Works|CodeSite</title>
+</svelte:head>
+<main>
+    <h1>How it works</h1>
+    <p> Navigating a website for the first time can be hard. Here is how you do it </p>
+    <br>
+
+    <h2>The Navigation Bars</h2>
+    <section class="bar-guide">
+        <div class="rightbox">
+            <p>The website consists of multiple tabs each representing a different page. Think of it like the pages of a book(or smt else idk). <br>
+                    <br>
+                    In order to flip through these tabs, you need to use the teal navigation bar to your left. Click on the tab title to switch to that tab.<br>
+                    <br>
+                    Try switching to the home screen then back like in the demonstration to the right
+            </p>
+            <image src="src/routes/how-it-works/dummygif.png", alt="demonstration gif">
+        </div>
+
+        <h3>Dropdowns</h3>
+
+        <p>Notice that the “Lessons” title is different……………… Once you open the lessons dropdown, there is 2 options, website and python. </p>
+    </section>
+
+    <h2>Organization</h2>
+    <p>This website has 4 pages. You are currently in the “How to use the website” tab so we won’t cover that here. Here is what the rest of the tabs they do</p>
+</main>
+
+<style>
+    section {
+        padding: 20px;
+        background-color: #b8bdb5;
+        border-radius: 50px;
+        width: 70%;
+        padding-right: 10rem;
+    }
+    .rightbox {
+        display: flex;
+        width: 80%
+    }
+    main {
+        padding-right: 10rem;
+    }
+</style>
--- src/routes/navigation/+page.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-	<title>How It Works|CodeSite</title>
-</svelte:head>
-
-<h1>How it works</h1>
